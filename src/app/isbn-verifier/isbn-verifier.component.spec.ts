a4c783c95a40b6082099d535184823932e55d1a3
Felipe Araujo <devaraujofelipe@gmail.com>
2023-11-22 17:58:03 +0100

    feat: add tests for isbn-verifier component

--- src/app/isbn-verifier/isbn-verifier.component.spec.ts
+++ src/app/isbn-verifier/isbn-verifier.component.spec.ts
@@ -18,4 +18,18 @@ describe('IsbnVerifierComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should validate the ISBN', () => {
+    component.isbnInput = '3-598-21507-X';
+    component.validateIsbn();
+    expect(component.isIsbnValid).toBe(true);
+    expect(component.validationResult).toBe(`${component.isbnInput} is ${component.isIsbnValid ? 'a valid' : 'an invalid'} ISBN number`);
+  });
+
+  it('should show an error message for invalid ISBN', () => {
+    component.isbnInput = '1234567890';
+    component.validateIsbn();
+    expect(component.isIsbnValid).toBe(false);
+    expect(component.validationResult).toBe(`${component.isbnInput} is ${component.isIsbnValid ? 'a valid' : 'an invalid'} ISBN number`);
+  });
 });
