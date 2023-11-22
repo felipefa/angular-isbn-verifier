d0b1db81fa82cb2cdabe97996427345344101572
Felipe Araujo <devaraujofelipe@gmail.com>
2023-11-22 17:11:39 +0100

    feat: add TailwindCSS and customise app layout

--- src/app/isbn-verifier/isbn-verifier.component.ts
+++ src/app/isbn-verifier/isbn-verifier.component.ts
@@ -9,20 +9,39 @@ import { IsbnService } from '../services/isbn.service';
 export class IsbnVerifierComponent {
   hasIsbnBeenValidated = false;
   isbnInput = '';
+  isbnInputError = '';
   isbnService: IsbnService;
   isIsbnValid = false;
+  validationResult = '';
 
   constructor() {
     this.isbnService = new IsbnService();
   }
 
   onChangeIsbnInput(event: any) {
-    this.isbnInput = event.target.value;
+    const newValue = event.target.value;
+
+    this.isbnInput = newValue;
+
+    if (newValue.length === 0) {
+      this.isbnInputError = 'Please enter an ISBN';
+    } else {
+      const hasNumberOrX = /^[0-9\-xX]+$/.test(newValue);
+
+      if (!hasNumberOrX) {
+        this.isbnInputError = 'ISBN must contain only digits or X';
+      } else {
+        this.isbnInputError = '';
+      }
+    }
+
     this.hasIsbnBeenValidated = false;
   }
 
   validateIsbn() {
     this.isIsbnValid = this.isbnService.isValid(this.isbnInput);
+
+    this.validationResult = `${this.isbnInput} is ${this.isIsbnValid ? 'a valid' : 'an invalid'} ISBN number`;
     this.hasIsbnBeenValidated = true;
   }
 }
